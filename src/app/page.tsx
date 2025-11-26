9fde8250efd3c573bdbb89cf1d86e736975329be
RenatoNunes22 <renato.mdn@outlook.com>
2025-11-26 19:26:20 -0300

    feat: Add initial home page with SEO metadata and Open Graph image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,30 @@
 import { LadingPage } from "@/templates/lading-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Site.Set",
+    description: "Sell your products as an affiliate in one place",
+    robots: {
+        index: true,
+        follow: true,
+    },
+    openGraph: {
+        title: "Site.Set",
+        description: "Sell your products as an affiliate in one place",
+        url: "https://sitesetblog.vercel.app/og-image.jpg",
+        siteName: "Site.Set",
+        type: "website",
+        locale: "en_US",
+        images: [
+            {
+                url: "https://sitesetblog.vercel.app/og-image.jpg",
+                width: 800,
+                height: 600,
+                alt: "Site.Set",
+            },
+        ],
+    },
+}
 
 export default function Home() {
     return <LadingPage />;
